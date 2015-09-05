3e530f03482078ce0bd7573f5e033f90a49d834f
caorong <lelouchcr@gmail.com>
2015-09-06 00:17:41 +0800

    add notify only once if watchlist changed

--- clientNotifier.py
+++ clientNotifier.py
@@ -4,6 +4,8 @@
 import os
 import argparse
 from subprocess import call
+import json
+import pickle
 
         
 import requests
@@ -15,14 +17,35 @@ def main():
             '-i', '--host', default='localhost', help='monitor host')
     parser.add_argument(
             '-p', '--port', default=9400, help='monitor post')
+    parser.add_argument(
+            '-d', '--debug', default=False, type=bool, help='monitor post')
     args = parser.parse_args()
 
     r = requests.get('http://'+args.host + ':' + args.port + '/monitor')
     wl = r.json()
-    # print(wl)
 
-    if wl:
-        call(["/usr/bin/osascript", "-e", "display notification \"live - {}\" with title \"sc2\" subtitle \"now streaming {}\"".format(','.join(wl),  "")])
+    if args.debug:
+        print(wl)
+        
+    _fname = "/tmp/sc2now"
+
+    # if file exists, and live list is same
+    # do not call notification
+
+    if os.path.isfile(_fname):
+        with open(_fname, 'rb') as f:
+            pdata = pickle.load(f)
+            if json.loads(pdata) != wl:
+                call_notifier(wl)
+    else:
+        call_notifier(wl)
+
+
+    with open(_fname, 'wb') as f:
+        pickle.dump(json.dumps(wl), f, 0)
+        
+def call_notifier(wl):
+    call(["/usr/bin/osascript", "-e", "display notification \"live - {}\" with title \"sc2\" subtitle \"now streaming {}\"".format(','.join(wl),  "")])
 
 
 if __name__ == '__main__':
