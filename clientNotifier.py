de51e10e0db7496a259247ecf4ae141a92ea2299
caorong <lelouchcr@gmail.com>
2015-10-07 22:14:37 +0800

    add add private key, when vps close ssh login

--- clientNotifier.py
+++ clientNotifier.py
@@ -29,6 +29,8 @@ def main():
             '-p', '--port', default=9400, help='monitor post')
     parser.add_argument(
             '-d', '--debug', default=False, type=bool, help='monitor post')
+    parser.add_argument(
+            '-id', '--identify', default='/root/.ssh/id_rsa', help='fabric remote vps ssh private key')
     args = parser.parse_args()
 
     r = requests.get('http://'+args.host + ':' + args.port + '/monitor')
@@ -49,14 +51,14 @@ def main():
             pdata = pickle.load(f)
             previous_wl = json.loads(pdata)
             if previous_wl != wl:
-                call_notifier(previous_wl, wl)
+                call_notifier(previous_wl, wl, args.identify)
     else:
-        call_notifier(None, wl)
+        call_notifier(None, wl, args.identify)
 
     with open(_fname, 'wb') as f:
         pickle.dump(json.dumps(wl), f, 0)
         
-def call_notifier(pwl, wl):
+def call_notifier(pwl, wl, identifiy_file=''):
     if OS == 'Darwin':
         call(["/usr/bin/osascript", "-e", "display notification \"live - {}\" with title \"sc2\" subtitle \"now streaming {}\"".format(','.join(wl),  "")])
     elif OS == 'Linux':
@@ -69,7 +71,7 @@ def call_notifier(pwl, wl):
             #  print(target, REMOTE_VPS_IP)
             # fix yourself
             #  print(["/usr/bin/python", CURR_DIR + '/fabfile.py', str(watchlist[target]), str(REMOTE_VPS_IP)])
-            call(["/usr/bin/python", CURR_DIR + '/fabfile.py', str(watchlist[target]), str(REMOTE_VPS_IP)])
+            call(["/usr/bin/python", CURR_DIR + '/fabfile.py', str(watchlist[target]), str(REMOTE_VPS_IP), str(identifiy_file)])
             #  call(["python", CURR_DIR + '/fabfile.py', str(watchlist[target]), str(REMOTE_VPS_IP)])
 
 if __name__ == '__main__':
